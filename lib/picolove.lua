34594269391fa7eefec28d96691e2ab9e10dc2cd
Andrew Anderson <andrewloveskitties@gmail.com>
2019-09-22 16:11:08 -0700

    Remove some trailing whitespace

--- lib/picolove.lua
+++ lib/picolove.lua
@@ -3,15 +3,15 @@
 -- <https://github.com/ftsf/picolove>
 
 -- Copyright (c) 2015 Jez Kabanov <thesleepless@gmail.com>
--- 
+--
 -- This software is provided 'as-is', without any express or implied
 -- warranty. In no event will the authors be held liable for any damages
 -- arising from the use of this software.
--- 
+--
 -- Permission is granted to anyone to use this software for any purpose,
 -- including commercial applications, and to alter it and redistribute it
 -- freely, subject to the following restrictions:
--- 
+--
 -- 1. The origin of this software must not be misrepresented; you must not
 --    claim that you wrote the original software. If you use this software
 --    in a product, an acknowledgement in the product documentation would be
